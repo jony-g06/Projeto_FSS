9f9d133c0b21ec40de3ff3c25cc43df3319935bd
Jony_Geezus <62430100+jony-g06@users.noreply.github.com>
2026-05-14 14:24:49 +0100

    Update projeto_fss.m
    Já feito:
    - Função de cálculo das diferenças entre amostras
    
    Por fazer:
    - Criar um ficheiro de referência
    - Definir um valor para a tolerância das diferenças e quantas amostras "diferentes" se pode pode considerar
    - Reconhecer o que foi dito
    - Definir o que o programa faz quando reconhece a palavra dita

--- projeto_fss.m
+++ projeto_fss.m
@@ -1,6 +1,31 @@
 %% Projeto de FSS
 %%
+load referencia.coiso
 
-% here it starts
-%here it follows
+% Continuar
 
+%% Cálculo da diferença entre o sinal recebido e o esperado:
+% Esta função calcula a diferença relativa entre os valores das frequências
+% presentes no áudio recebido pelo microfone e as frequências presentes na
+% amostra de áudio usada como referência.
+%
+% Entradas:
+% tent: Vetor das frequências presentes no aúdio recebido, obtido através
+% de uma transformada de Fourrier
+%
+% base: Vetor das frequências presentes no aúdio de referência, obtido 
+% através de uma transformada de Fourrier
+%
+% Saída:
+% dif: Vetor que contêm os valores das diferenças relativas entre os
+% valores recebidos pelo microfone e os da amostra-base, em %
+%
+% Nota: Nesta função, considera-se apenas metade do comprimento dos vetores
+% de entrada, dada a simetria que existe a partir do ponto médio deste
+
+function dif = diferenca(tent, base)
+    dif = zeros(0:length(base)/2);
+    for k = 1:length(base)/2
+        dif(k) = abs(tent(k) - base(k))/base(k);
+    end
+end
