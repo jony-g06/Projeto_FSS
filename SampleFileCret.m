765767304855cedd2b36719798fcb37b019774ac
Lemon_green <gabrielmnogueira13@ua.pt>
2026-05-18 18:35:27 +0100

    Update SampleFileCret.m
    now suporting receber() function

--- SampleFileCret.m
+++ SampleFileCret.m
@@ -6,8 +6,10 @@
 % 
 % 
 
-function SampleFileCret( nameoffile, audiof)
-    [xq, fa]= audioread(audiof)
+function SampleFileCret( nameoffile)
+    [file, fa] = receber();
+    %[xq, fa]= audioread()
+    xq = getaudiodata(file)
     writematrix(xq, nameoffile)
     fastr = append(nameoffile, 'fa')
     writematrix(fa, fastr)
